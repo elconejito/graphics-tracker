c5e7e7c138f423e48dd54549c5c0042aab0601ce
elconejito <harvey@harvsworld.com>
2015-02-08 13:05:30 -0500

    Create migration for Jobs table

---
+++ database/migrations/2015_02_08_164904_create_jobs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('jobs', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('graphic');
+			$table->integer('project_id');
+			$table->integer('user_id');
+			$table->integer('duration');
+			$table->timestamp('job_start');
+			$table->timestamp('job_end');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('jobs');
+	}
+
+}
