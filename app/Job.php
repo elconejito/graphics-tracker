27fea75400f4a93bda10ab2af4a0d5dd8e067ea7
elconejito <harvey@harvsworld.com>
2015-02-11 09:16:20 -0500

    Job->New flag set on store Jobs@index only shows logged in user's jobs

--- app/Job.php
+++ app/Job.php
@@ -6,12 +6,29 @@ class Job extends Model {
 
 	protected $fillable = [ 'graphic' ];
 
+	/**
+	 * @return BelongsTo
+	 */
 	public function project() {
 		return $this->belongsTo('App\Project');
 	}
-	
+
+	/**
+	 * @return BelongsTo
+	 */
 	public function owner() {
 		return $this->belongsTo('App\User', 'user_id');
+	}
+
+	/**
+	 *  Set the 'new' flag based on whether or not any other graphic has the same name
+	 *  regardless of the owner.
+	 *
+	 */
+	public function setNew() {
+		$count = Job::where('graphic', '=', $this->graphic)->count();
+
+		$this->new = ( $count > 0 ? false:true );
 	}
 
 }
