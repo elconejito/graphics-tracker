672c4f45f264611db23811e222d956d1cb781a22
Harvey Ramos <harvey@harvsworld.com>
2015-02-24 12:23:18 -0500

    formatting the report table

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -14,6 +14,8 @@
                     <thead>
                         <tr>
                             <th>Name</th>
+                            <th>New</th>
+                            <th>Edit</th>
                             <th>Total</th>
                         </tr>
                     </thead>
@@ -24,20 +26,28 @@
                         </tr>
                         @else
                             <?php
+                            $total_new = 0;
+                            $total_edit = 0;
                             $total_graphics = 0;
                             ?>
                             @foreach ( $projects as $project )
                         <tr>
                             <td>{{ $project->getName() }}</td>
+                            <td>{{ $project->jobs()->new()->$timeframe()->count() }}</td>
+                            <td>{{ $project->jobs()->edit()->$timeframe()->count() }}</td>
                             <td>{{ $project->jobs()->$timeframe()->count() }}</td>
                         </tr>
                             <?php 
+                            $total_new = $total_new + $project->jobs()->new()->$timeframe()->count();
+                            $total_edit = $total_edit + $project->jobs()->edit()->$timeframe()->count();
                             $total_graphics = $total_graphics + $project->jobs()->$timeframe()->count();
                             ?>
                             @endforeach
-                        <tr>
-                            <td></td>
-                            <td>{{ $total_graphics }}</td>
+                        <tr class="info">
+                            <td>TOTAL GRAPHICS</td>
+                            <td>{{ $total_new }}</td>
+                            <td>{{ $total_edit }}</td>
+                            <td><span class="badge">{{ $total_graphics }}</span></td>
                         </tr>
                         @endif
                     </tbody>
