1946cefa13b0f349aa4bf7d140756b48b25040d5
Harvey Ramos <harvey@harvsworld.com>
2015-02-24 12:43:19 -0500

    added auth filter to reports

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 use App\Project;
 
 class ReportsController extends Controller {
+	
+	public function __construct() {
+		$this->middleware('auth');
+	}
 
 	/**
 	 * Display a listing of the resource.
